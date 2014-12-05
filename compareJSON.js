94a40eeaa45b965debefbfeb31a2c5f707153e2e
zengcl <zengcl@ucweb.com>
2014-12-05 15:58:22 +0800

    version 2， 需解决顶层对象结构问题

--- compareJSON.js
+++ compareJSON.js
@@ -10,7 +10,7 @@ var META_TYPE = {
 function copyJSON(obj) {
 	var obj_rev = {};
 	if(type(obj) === "object") {
-		for(prop in obj) {
+		for(var prop in obj) {
 			obj_rev[prop] = copyJSON(obj[prop]);
 		}
 	} else if(type(obj) === "array") {
@@ -30,7 +30,7 @@ function copyJSON(obj) {
 function extendJSON(obj_rev, obj1, obj2) {
 	obj2 = obj2 ? obj2 : {};
 	if(type(obj1) === "object" && type(obj2) === "object") {
-		for(prop in obj1) {
+		for(var prop in obj1) {
 			if(typeof obj1[prop] in META_TYPE) {
 				obj_rev[prop] = obj1[prop];
 			} else if(obj1[prop] instanceof Array) {
@@ -44,22 +44,16 @@ function extendJSON(obj_rev, obj1, obj2) {
 			}
 		}
 
-		for(prop in obj2) {
+		for(var prop in obj2) {
 			if(typeof obj2[prop] in META_TYPE) {
 				obj_rev[prop] = obj2[prop];
 			} else if(obj2[prop] instanceof Array) {
 				if(prop in obj_rev) {
-					// obj2[prop].forEach(function(val, index) {
-					// 	if(obj_rev[prop].indexOf(val) === -1) {
-					// 		obj_rev[prop].push(val);
-					// 	}
-					// });
 					var ary_tmp = new Array(obj_rev[prop].length),
 						prop_tmp = "";
 					obj_rev[prop].forEach(function(val, index, ary) {
 						ary_tmp[index] = json2Str(copyJSON(val));
 					});
-					//var ary_tmp2 = new Array(obj2[prop].length);
 					obj2[prop].forEach(function(val, index, ary) {
 						//ary_tmp2[index] = json2Str(copyJSON(val));
 						prop_tmp = json2Str(copyJSON(val));
@@ -69,7 +63,6 @@ function extendJSON(obj_rev, obj1, obj2) {
 					});
 
 				} else {
-					//obj_rev[prop] = obj2[prop];
 					obj_rev[prop] = new Array(obj2[prop].length);
 					obj2[prop].forEach(function(val, index, ary) {
 						obj_rev[prop][index] = copyJSON(val);
@@ -85,48 +78,106 @@ function extendJSON(obj_rev, obj1, obj2) {
 	}
 }
 
-function compareJSON(obj_rev, obj1, obj2) {
-	//$.extend(true, obj_rev, obj1, obj2);
-	//console.log(obj_rev);
+// function compareJSON(obj_rev, obj1, obj2) {
+// 	//$.extend(true, obj_rev, obj1, obj2);
+// 	//console.log(obj_rev);
 	
-	for(prop in obj_rev) {
-		if(prop !== "type") {
-			//console.log(prop);
-			if(prop in obj2 && !(prop in obj1)) {
-				//obj_rev[prop] = $.extend({}, obj2[prop]);
-				obj_rev[prop].compare_options = "add";
-			} else if(!(prop in obj2) && prop in obj1) {
-				//obj_rev[prop] = $.extend({}, obj1[prop]);
-				obj_rev[prop].compare_options = "del";
-			} else {
-				// if($.type(obj_rev[prop]) in {"string":"", "number":"", "boolean":"", "null":"", "undefined":""}) {
-				// 	//console.log(prop);
-				// 	//console.log(obj2[prop], obj1[prop]);
-				// 	if(obj2[prop] === obj1[prop]) {
-				// 		obj_rev.compare_options = "eq";
-				// 	} else {
-				// 		obj_rev.compare_options = "mod";
-				// 	}
-				// } else {
-				// 	compareJSON(obj_rev[prop], obj1[prop], obj2[prop]);
-				// }
-				if(obj1[prop].type !== obj2[prop].type) {
-					obj_rev[prop].compare_options = "mod";
-				} else {
-					if(obj1[prop].type === "text") {
-						if(obj1.data === obj2.data) {
-							obj_rev[prop].compare_options = "eq";
-						} else {
-							obj_rev[prop].compare_options = "mod";
-						}
+// 	for(var prop in obj_rev) {
+// 		if(prop !== "type") {
+// 			//console.log(prop);
+// 			if(prop in obj2 && !(prop in obj1)) {
+// 				//obj_rev[prop] = $.extend({}, obj2[prop]);
+// 				obj_rev[prop].compare_options = "add";
+// 			} else if(!(prop in obj2) && prop in obj1) {
+// 				//obj_rev[prop] = $.extend({}, obj1[prop]);
+// 				obj_rev[prop].compare_options = "del";
+// 			} else {
+// 				// if($.type(obj_rev[prop]) in {"string":"", "number":"", "boolean":"", "null":"", "undefined":""}) {
+// 				// 	//console.log(prop);
+// 				// 	//console.log(obj2[prop], obj1[prop]);
+// 				// 	if(obj2[prop] === obj1[prop]) {
+// 				// 		obj_rev.compare_options = "eq";
+// 				// 	} else {
+// 				// 		obj_rev.compare_options = "mod";
+// 				// 	}
+// 				// } else {
+// 				// 	compareJSON(obj_rev[prop], obj1[prop], obj2[prop]);
+// 				// }
+// 				if(obj1[prop].type !== obj2[prop].type) {
+// 					obj_rev[prop].compare_options = "mod";
+// 				} else {
+// 					if(obj1[prop].type === "text") {
+// 						if(obj1.data === obj2.data) {
+// 							obj_rev[prop].compare_options = "eq";
+// 						} else {
+// 							obj_rev[prop].compare_options = "mod";
+// 						}
+// 					} else {
+// 						compareJSON(obj_rev[prop], obj1[prop], obj2[prop]);
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return true
+// }
+
+function compareJSON(obj_rev, obj1, obj2) {
+	if(obj1.type !== obj2.type) {
+		obj_rev.compare_options = "type_mod";
+		obj_rev.type1 = obj1.type;
+		obj_rev.data1 = obj1.data;
+		obj_rev.type = obj2.type;
+		obj_rev.data = obj2.data;
+	} else {
+		switch(obj1.type) {
+			case "object": {
+				for(var prop in obj_rev.data) {
+					if(prop in obj2.data && !(prop in obj1.data)) {
+						obj_rev.data[prop].compare_options = "add";
+					} else if(!(prop in obj2.data) && prop in obj1.data) {
+						obj_rev.data[prop].compare_options = "del";
 					} else {
-						compareJSON(obj_rev[prop], obj1[prop], obj2[prop]);
+						if(equalJSON(obj1.data[prop], obj2.data[prop])) {
+							obj_rev.data[prop].compare_options = "eq";
+						} else {
+							compareJSON(obj_rev.data[prop], obj1.data[prop], obj2.data[prop]);
+						}
 					}
 				}
-			}
+			} break;
+			case "list": {
+				// obj_rev.data.forEach(function(val, index, ary) {
+				// 	compareJSON(obj_rev.data[index], obj1.data[index], obj2.data[index]);
+				// });
+				var max_len = obj1.data.length > obj2.data.length ? obj1.data.length : obj2.data.length;
+				var min_len = obj1.data.length < obj2.data.length ? obj1.data.length : obj2.data.length;
+				obj_rev.data = copyJSON(obj2.data);
+				obj_rev.data = obj_rev.data.concat(copyJSON(obj1.data.slice(obj2.data.length)));
+				for(var i = 0; i < min_len; i++) {
+					compareJSON(obj_rev.data[i], obj1.data[i], obj2.data[i]);
+				}
+				if(obj2.data.length === max_len) {
+					for(var i = min_len; i < max_len; i++) {
+						obj_rev.data[i].compare_options = "add";
+					}
+				} else {
+					for(var i = min_len; i < max_len; i++) {
+						obj_rev.data[i].compare_options = "del";
+					}
+				}
+			} break;
+			case "text": {
+				if(obj1.data === obj2.data) {
+					obj_rev.compare_options = "eq";
+				} else {
+					obj_rev.compare_options = "mod";
+					obj_rev.data1 = obj1.data;
+				}
+			} break;
+			default: {}
 		}
 	}
-	return true
 }
 
 var obj1 = {
@@ -144,11 +195,20 @@ var obj1 = {
 					"data": "Tom"
 				},
 				{
-					"type": "object",
+					"type": "list",
 					"data": [
-						"Chenglee",
-						"Cherlse",
-						"Tommmmmm"
+						{
+							"type": "text",
+							"data": "Chenglee"
+						},
+						{
+							"type": "text",
+							"data": "cherlse"
+						},
+						{
+							"type": "text",
+							"data": "Tommmmmm"
+						}
 					]
 				}
 			]
@@ -160,6 +220,75 @@ var obj1 = {
 		"interest": {
 			"type": "text",
 			"data": "swimming"
+		},
+		"info": {
+			"type": "object",
+			"data": {
+				"birth": {
+					"type": "text",
+					"data": "1992.10.25"
+				},
+				"name": {
+					"type": "text",
+					"data": "Chenglee"
+				},
+				"id": {
+					"type": "text",
+					"data": "GZ4300"
+				},
+				"gf": {
+					"type": "object",
+					"data": {
+						"name": {
+							"data": "zhuer",
+							"type": "text"
+						},
+						"sex": {
+							"data": "female",
+							"type": "text"
+						},
+						"age": {
+							"data": "23",
+							"type": "text"
+						},
+						"interest": {
+							"type": "list",
+							"data": [{
+								"type": "text",
+								"data": "swimming"
+							}, {
+								"type": "text",
+								"data": "make"
+							}, {
+								"type": "object",
+								"data": {
+									"name": {
+										"type": "text",
+										"data": "fighting"
+									},
+									"good": {
+										"type": "text",
+										"data": "no"
+									}
+								}
+							}]
+						}
+					}
+				}
+			}
+		},
+		"test": {
+			"type": "object",
+			"data": {
+				"data for test": {
+					"type": "text",
+					"data": "DAtA FOR TEST"
+				},
+				"data for test33": {
+					"type": "text",
+					"data": "DAtA FOR TEST33"
+				}
+			}
 		}
 	}
 };
@@ -190,6 +319,59 @@ var obj2 = {
 		"sex": {
 			"type": "text",
 			"data": "male"
+		},
+		"info": {
+			"type": "object",
+			"data": {
+				"birth": {
+					"type": "text",
+					"data": "1992.10.25"
+				},
+				"name": {
+					"type": "text",
+					"data": "Cherlse"
+				},
+				"id": {
+					"type": "text",
+					"data": "GZ4300"
+				},
+				"gf": {
+					"type": "object",
+					"data": {
+						"name": {
+							"data": "Huangying",
+							"type": "text"
+						},
+						"sex": {
+							"data": "female",
+							"type": "text"
+						},
+						"age": {
+							"data": "23",
+							"type": "text"
+						},
+						"interest": {
+							"type": "list",
+							"data": [{
+								"type": "url",
+								"data": "swimming"
+							}, {
+								"type": "text",
+								"data": "make"
+							}]
+						}
+					}
+				}
+			}
+		},
+		"test": {
+			"type": "object",
+			"data": {
+				"data for test": {
+					"type": "text",
+					"data": "DATA FOR TEST"
+				}
+			}
 		}
 	}
 };
@@ -241,7 +423,7 @@ function _json2Str(obj) {
 	var props = [],
 		strs = [];
 	if(type(obj) === "object") {
-		for(prop in obj) {
+		for(var prop in obj) {
 			props.push(prop);
 		}
 		props.sort();
@@ -271,10 +453,16 @@ function _json2Str(obj) {
 }
 
 function json2Str(obj) {
-	var obj_tmp = obj;
+	var obj_tmp = copyJSON(obj);
 	if(type(obj_tmp) === "object") return "{" + _json2Str(obj_tmp) + "}";
 	if(type(obj_tmp) === "array") return "[" + _json2Str(obj_tmp) + "]";
 	return "" + obj_tmp;
+}
+
+function equalJSON(obj1, obj2) {
+	var str1 = json2Str(obj1),
+		str2 = json2Str(obj2);
+	return str1 === str2;		
 }
 // console.log(obj);
 // console.log(json2Str(obj));
@@ -293,4 +481,6 @@ function json2Str(obj) {
 
 var obj = {};
 extendJSON(obj, obj1, obj2);
-console.log(obj)
+compareJSON(obj, obj1, obj2);
+console.log(json2Str(obj));
+// console.log(json2Str(obj));
